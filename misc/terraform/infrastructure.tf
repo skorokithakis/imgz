b7dc94993036580e06ec360654f5c744787ca650
Stavros Korokithakis <hi@stavros.io>
2019-11-18 22:43:22 +0200

    Add mail DNS settings

--- misc/terraform/infrastructure.tf
+++ misc/terraform/infrastructure.tf
@@ -52,6 +52,44 @@ resource "cloudflare_record" "www" {
   value="${var.ipv4_ip}"
 }
 
+resource "cloudflare_record" "mg" {
+  zone_id=var.zone
+  type="CNAME"
+  name="email.mail"
+  value="eu.mailgun.org"
+}
+
+resource "cloudflare_record" "spf" {
+  zone_id=var.zone
+  type="TXT"
+  name="mail"
+  value="v=spf1 include:eu.mailgun.org ~all"
+}
+
+resource "cloudflare_record" "mx_domainkey" {
+  zone_id=var.zone
+  type="TXT"
+  name="pic._domainkey.mail"
+  value="k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6m6Fr5fmS9pVGiKOgEG57MnR/r/FtLaUdG93u7rC+HIiqjeUPMwPx4Aya7TDAE03/p4BQdZ5w3WQpowsLp7tzSnNhVLAe8jiMzCJ610qEO7j/ys5TeI9SPt1nGaccLbhCcawz3T0KKV4sHrKxsZOnayDuuj3QMhVJn4AbzX7JYrbrg3NfP/d8fWztmBk2/ZvjWeYh25KoSoJ9KusQNqPbOP6ACIx9VWDgtfpcBNPFQoVk/iVhwuLrk2bR4PFQP8wyhcoJA3GDK3vWJ1xO+ddAx+LCihVRswpgQobe33WVzjtiRHn8aaadDOi1AF5xl0gOWEM9qasZ6Y8J5JU2O0lVQIDAQAB"
+}
+
+resource "cloudflare_record" "mxa" {
+  zone_id=var.zone
+  type="MX"
+  name="mail"
+  priority="10"
+  value="mxa.eu.mailgun.org"
+}
+
+resource "cloudflare_record" "mxb" {
+  zone_id=var.zone
+  type="MX"
+  name="mail"
+  priority="10"
+  value="mxb.eu.mailgun.org"
+}
+
+
 resource "cloudflare_page_rule" "cache_images" {
   zone_id = var.zone
   target = "https://imgz.org/i*"
