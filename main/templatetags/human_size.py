7efb37fe1d1ebbf926544464ca7bbbf88bf5c0e6
Stavros Korokithakis <hi@stavros.io>
2019-11-20 18:50:11 +0200

    Fix size display

--- main/templatetags/human_size.py
+++ main/templatetags/human_size.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 from hurry.filesize import size
 
@@ -7,4 +9,4 @@ register = template.Library()
 @register.filter
 def human_size(byte_size):
     s = size(byte_size)
-    return f"{s[:-1]} {s[-1]}B"
+    return re.sub(r"(\d+)(\w+)", r"\1 \2", s)
